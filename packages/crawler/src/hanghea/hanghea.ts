382eaccb0eea6c05fc22209cf586964b4335aead
진돌 <wlstjrghdud@naver.com>
2025-08-03 20:34:15 +0900

    fix hanghea => hanghae 오타 수정

--- packages/crawler/src/hanghea/hanghea.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { HangheaService } from './hanghea.service';
-
-@Module({
-  imports: [],
-  providers: [HangheaService],
-  exports: [HangheaService],
-})
-export class HangheaModule {}
