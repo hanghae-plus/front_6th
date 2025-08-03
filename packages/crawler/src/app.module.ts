cd6daa559222af417a0f5cfeafa54b8f847501fd
진돌 <wlstjrghdud@naver.com>
2025-08-03 18:56:39 +0900

    feat(crawler): integrate Hanghea module into app

--- packages/crawler/src/app.module.ts
+++ packages/crawler/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppService } from './app.service';
 import { GithubModule } from './github/github.module';
+import { HangheaModule } from './hanghea/hanghea';
 
 @Module({
-  imports: [GithubModule, ConfigModule.forRoot()],
+  imports: [GithubModule, HangheaModule, ConfigModule.forRoot()],
   providers: [AppService],
 })
 export class AppModule {}
